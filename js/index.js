ce3f45d4212beff767cf1dfe9b7fa8e0de03e012
aallenCR808 <267158274+aallenCR808@users.noreply.github.com>
2026-08-17 17:23:21 -0700

    ja added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const body = document.body;
+const footer = document.createElement("footer");
+body.appendChild(footer);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+const copyright = document.createElement("p");
+copyright.innerHTML = `\u00A9 Apeh Clary ${thisYear}`;
+footer.appendChild(copyright);
+
+const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
+const skillsSection = document.querySelector("#Skills");
+const skillsList = skillsSection.querySelector("ul");
+
+for (let skillName of skills) {
+  const skill = document.createElement("li");
+  skill.innerText = skillName;
+  skillsList.appendChild(skill);
+}
