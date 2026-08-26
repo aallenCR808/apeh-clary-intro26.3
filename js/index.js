0f03064205e649761ebf4846c9ea5e7a830b8aab
aallenCR808 <267158274+aallenCR808@users.noreply.github.com>
2026-08-26 16:48:16 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,17 +1,9 @@
-const body = document.body;
-const footer = document.createElement("footer");
-body.appendChild(footer);
-
-const today = new Date();
-const thisYear = today.getFullYear();
-const copyright = document.createElement("p");
-copyright.innerHTML = `\u00A9 Apeh Clary ${thisYear}`;
-footer.appendChild(copyright);
-
+// skills list
 const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
 const skillsSection = document.querySelector("#Skills");
 const skillsList = skillsSection.querySelector("ul");
 
+// for loop of skills to add each skill to the list
 for (let i = 0; i < skills.length; i++) {
   let skill = document.createElement("li");
   skill.textContent = skills[i];
@@ -24,3 +16,62 @@ for (let i = 0; i < skills.length; i++) {
 //   skill.innerText = skillName;
 //   skillsList.appendChild(skill);
 // }
+
+// message form
+let messageForm = document.forms["leave_message"];
+
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let name = event.target.usersName.value;
+  let email = event.target.usersEmail.value;
+  let message = event.target.usersMessage.value;
+
+  let messageSection = document.querySelector("#messages");
+  let messageList = messageSection.querySelector("ul");
+
+  // create message section list on website
+  let newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="mailto:${email}">${name}</a>
+  <span>${message}</span>`;
+
+  // add new message in messages section
+  messageList.appendChild(newMessage);
+
+  console.log(name, email, message);
+
+  messageForm.reset();
+
+  // create remove button, add 'remove' button to have remove message option
+  let removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.type = "button";
+
+  removeButton.addEventListener("click", function () {
+    let entry = removeButton.parentNode;
+    entry.remove();
+
+    if (document.querySelector("#messages>ul").children.length > 0)
+      document.querySelector("#messages").style.display;
+    else document.querySelector("#messages").style.display = "none";
+  });
+
+  newMessage.appendChild(removeButton);
+
+  if (document.querySelector("#messages>ul").children.length > 0)
+    document.querySelector("#messages").style.display = "block";
+  else document.querySelector("#messages").style.display = "none";
+
+  messageForm.reset();
+});
+
+// footer: copyright, name and current date
+const body = document.body;
+const footer = document.createElement("footer");
+body.appendChild(footer);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+const copyright = document.createElement("p");
+copyright.innerHTML = `\u00A9 Apeh Clary ${thisYear}`;
+footer.appendChild(copyright);
