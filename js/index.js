07cc6f29f4d3f76b8f8c3e8616322db80ebffc82
aallenCR808 <267158274+aallenCR808@users.noreply.github.com>
2026-09-14 22:21:50 -0700

    final project changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// skills list
+// skills section list
 const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
 const skillsSection = document.querySelector("#Skills");
 const skillsList = skillsSection.querySelector("ul");
@@ -9,6 +9,37 @@ for (let i = 0; i < skills.length; i++) {
   skill.textContent = skills[i];
   skillsList.appendChild(skill);
 }
+
+// Projects section: GitHub repositories in projects section GitHub API
+let main = document.querySelector("main");
+let repositories = [];
+
+fetch("https://api.github.com/users/aallenCR808/repos")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Request failed");
+    }
+    return response.json();
+  })
+
+  .then((data) => {
+    repositories = data;
+    console.log(repositories);
+
+    //assign data to webpage
+    const projectSection = document.querySelector("#Projects");
+    const projectList = projectSection.querySelector("ul");
+
+    for (let i = 0; i < repositories.length; i++) {
+      let project = document.createElement("li");
+      project.innerText = repositories[i]["name"];
+      projectList.appendChild(project);
+    }
+  })
+
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
 
 // message form
 let messageForm = document.forms["leave_message"];
@@ -28,9 +59,9 @@ messageForm.addEventListener("submit", function (event) {
   newMessage.innerHTML = `<a href="mailto:${email}">${name}</a>
   <span>${message}</span>`;
 
-  // create remove button, add 'remove' button to have remove message option
+  // create remove button
   let removeButton = document.createElement("button");
-  removeButton.textContent = "remove";
+  removeButton.textContent = "Remove";
   removeButton.type = "button";
 
   removeButton.addEventListener("click", function () {
@@ -42,12 +73,11 @@ messageForm.addEventListener("submit", function (event) {
     else document.querySelector("#messages").style.display = "none";
   });
 
-  newMessage.appendChild(removeButton);
   // add new message in messages section
+  newMessage.appendChild(removeButton);
   messageList.appendChild(newMessage);
   messageForm.reset();
 });
-
 // footer: copyright, name and current date
 const body = document.body;
 const footer = document.createElement("footer");
@@ -58,37 +88,3 @@ const thisYear = today.getFullYear();
 const copyright = document.createElement("p");
 copyright.innerHTML = `\u00A9 Apeh Clary ${thisYear}`;
 footer.appendChild(copyright);
-
-// console.log(fetch("https://api.github.com/users/aallenCR808/repos"));
-
-let main = document.querySelector("main");
-let repositories = [];
-
-fetch("https://api.github.com/users/aallenCR808/repos")
-  .then((response) => {
-    //chain a then method to fetch
-
-    if (!response.ok) {
-      throw new Error("Request failed");
-    }
-    return response.json(); //parse the response as JSON, call and pass it a function that returns the response JSON data
-  })
-
-  .then((data) => {
-    repositories = data; //do something with the data
-    console.log(repositories);
-
-    //assign data to webpage
-    const projectSection = document.querySelector("#Projects");
-    const projectList = projectSection.querySelector("ul");
-
-    for (let i = 0; i < repositories.length; i++) {
-      let project = document.createElement("li");
-      project.innerText = repositories[i]["name"];
-      projectList.appendChild(project);
-    }
-  })
-
-  .catch((error) => {
-    console.error("An error occurred:", error);
-  });
